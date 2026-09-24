a5b66057c706b986d1e02d5212c1bbf736b76bbd
Guilherme Messias de Sousa Silva <guilherme.messias.ssilva@gmail.com>
2026-09-24 13:20:03 -0300

    Realizado a criação do logo e topo do site

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,9 @@
+from multiprocessing import context
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Guilherme Messias'})
 
